cbb3ffaa1688c6f9b631f921653c38271c1b2205
SamehArmouche <sa74@alu.ua.es>
2021-11-23 23:45:34 +0100

    Consultar municipios hecho

--- ASV-Api/src/routes/index.js
+++ ASV-Api/src/routes/index.js
@@ -1,24 +1,42 @@
 const { Router } = require('express');
 const router = Router();
+const fetch = require('cross-fetch')
 
-router.get('/municipios', (req, res) =>{
-  if(req.query.prefMpio===undefined){
-    res.status(400);
-    res.send("Error");
-  }else{
-    res.status(200);
-    res.send("Ok");
+const API_URL = "https://opendata.aemet.es/opendata/api/maestro/municipios" +
+  "?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzYTc0QGFsdS51YS5lcyIsImp0aSI6ImY"+
+  "2OWYwOTVlLThhNjItNGMxZi1hZDJlLWI3YzYxZjA2MWM1NCIsImlzcyI6IkFFTUVUIiwiaWF0I" + 
+  "joxNjM3NjcwODkyLCJ1c2VySWQiOiJmNjlmMDk1ZS04YTYyLTRjMWYtYWQyZS1iN2M2MWYwNjFjNTQiLCJyb2xlIjoiIn0.ljG2L_wcF4NvQFAipiQ5AvXXMN9Eg-LS1roprTMs0QI";
+
+router.get('/municipios', async (req, res) =>{
+  if(!req.query.prefMpio){
+    res.status(400).json({mensaje: "Bad request - falta el parametro prefMpio"});
+    return;
   }
+  
+  fetch(API_URL)
+  .then(response => response.arrayBuffer())
+  .then(buffer => {
+    let decoder = new TextDecoder("iso-8859-1");
+    let data = JSON.parse(decoder.decode(buffer));
+    const result = data.filter(item =>  item.nombre.includes(req.query.prefMpio));
+    const response=[];
+    Object.keys(result).forEach(function(key) {
+      item = {}
+      item ["nombre"] = result[key].nombre;
+      item ["id"] = result[key].id.substring(2);;
+      response.push(item);
+    })
+    res.status(200).send(response)
+  })
+  .catch(err => res.status(400).json({mensaje: err}));
 });
 
 router.get('/prediccion', (req, res) =>{
-  if(req.query.idMpio===undefined){
-    res.status(400);
-    res.send("Error");
-  }else{
-    res.status(200);
-    res.send("Ok");
+  if(!req.query.idMpio){
+    res.status(400).json({mensaje: "Bad request - falta el parametro idMpio"});
+    return;
   }
+    res.status(200).send("Ok");
 });
 
 
