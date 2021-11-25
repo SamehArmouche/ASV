f27fb7f6f4641bda42ffae5502a1d80cf3be7ac9
SamehArmouche <sa74@alu.ua.es>
2021-11-25 14:36:56 +0100

    Corregiendo la estructura de salida

--- ASV-Api/src/utils/extractData.js
+++ ASV-Api/src/utils/extractData.js
@@ -6,23 +6,20 @@ getData = async (url) => {
   .then(buffer => {
     let decoder = new TextDecoder("iso-8859-1");
     let data = JSON.parse(decoder.decode(buffer));
-    const dataRequierida=[];
+    const dataRequierida={};
     const dataPrecipitacion=[];
     const fechaDiaSiguiente=data[0].prediccion.dia[1].fecha;
     
     const result = data.filter(item =>  {
-      itemTemperatura={}
       itemPrecipitacion={}
       precipitacion={}
-      itemTemperatura["mediaTemperatura"] = (item.prediccion.dia[1].temperatura.maxima+item.prediccion.dia[1].temperatura.maxima)/2;
+      dataRequierida["mediaTemperatura"] = (item.prediccion.dia[1].temperatura.maxima+item.prediccion.dia[1].temperatura.maxima)/2;
       item.prediccion.dia[1].probPrecipitacion.filter(precipitacionItem =>  {
         itemPrecipitacion["probabilidad"]= precipitacionItem.value;
         itemPrecipitacion["periodo"]= precipitacionItem.periodo; 
         dataPrecipitacion.push(itemPrecipitacion)
       });
-      precipitacion['probPrecipitacion'] = dataPrecipitacion;
-      dataRequierida.push(itemTemperatura)
-      dataRequierida.push(precipitacion);
+      dataRequierida['probPrecipitacion'] = dataPrecipitacion;
     });
     return dataRequierida;
   })
